de1b0e4adae7c984a46e166ae4f4230290f36e88
Pavel Z <tiddiebiter@gmail.com>
2023-04-28 09:06:07 +0500

    Add katakana

--- ui-shared/src/commonMain/kotlin/space/compoze/hiero/ui/shared/stacknavigation/DefaultStackNavigationComponent.kt
+++ ui-shared/src/commonMain/kotlin/space/compoze/hiero/ui/shared/stacknavigation/DefaultStackNavigationComponent.kt
@@ -37,7 +37,7 @@ class DefaultStackNavigationComponent(
                         componentContext,
                         storeFactory,
                         this,
-                        "2"
+                        "katakana"
                     )
                 )
 
