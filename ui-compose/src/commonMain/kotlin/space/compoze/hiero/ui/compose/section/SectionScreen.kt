8fc825eb0b50f04506f848b25f8445ee5991a2b3
Pavel Z <tiddiebiter@gmail.com>
2023-05-06 09:16:43 +0500

    Add buttons

--- ui-compose/src/commonMain/kotlin/space/compoze/hiero/ui/compose/section/SectionScreen.kt
+++ ui-compose/src/commonMain/kotlin/space/compoze/hiero/ui/compose/section/SectionScreen.kt
@@ -3,6 +3,14 @@
 
 package space.compoze.hiero.ui.compose.section
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.animation.expandHorizontally
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleIn
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,15 +18,17 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.calculateEndPadding
 import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyGridScope
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Menu
+import androidx.compose.material.icons.rounded.MoreVert
 import androidx.compose.material.icons.rounded.PlayArrow
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
@@ -26,21 +36,29 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Popup
+import com.arkivanov.decompose.extensions.compose.jetbrains.stack.animation.fade
+import com.arkivanov.decompose.value.MutableValue
+import com.arkivanov.decompose.value.Value
 import space.compoze.hiero.domain.collectionitem.enums.CollectionItemType
 import space.compoze.hiero.domain.section.model.SectionModel
 import space.compoze.hiero.ui.compose.utils.subscribeAsState
@@ -69,24 +87,10 @@ private fun SectionContent(component: SectionComponent, content: SectionState.Co
     Scaffold(
         modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
         topBar = {
-            TopAppBar(
-                scrollBehavior = scrollBehavior,
-                navigationIcon = {
-                    IconButton({
-                        component.navigateBack()
-                    }) {
-                        Icon(Icons.Rounded.ArrowBack, "Back")
-                    }
-                },
-                title = {
-                    Text("${content.collection.title}: ${
-                        if (content.sections.size == 1) {
-                            content.sections[0].title
-                        } else {
-                            "All"
-                        }
-                    }")
-                },
+            SectionTopBar(
+                component,
+                content,
+                scrollBehavior
             )
         },
         floatingActionButton = {
@@ -118,11 +122,93 @@ private fun SectionContent(component: SectionComponent, content: SectionState.Co
     }
 }
 
+@OptIn(ExperimentalAnimationApi::class)
+@Composable
+private fun SectionTopBar(
+    component: SectionComponent,
+    content: SectionState.Content,
+    scrollBehavior: TopAppBarScrollBehavior,
+) {
+
+    val isMenuOpenValue = remember { MutableValue(false) }
+    val isMenuOpen by isMenuOpenValue.subscribeAsState()
+
+    TopAppBar(
+        scrollBehavior = scrollBehavior,
+        navigationIcon = {
+            IconButton({
+                component.navigateBack()
+            }) {
+                Icon(Icons.Rounded.ArrowBack, "Back")
+            }
+        },
+        title = {
+            Text(
+                "${content.collection.title}: ${
+                    if (content.sections.size == 1) {
+                        content.sections[0].title
+                    } else {
+                        "All"
+                    }
+                }"
+            )
+        },
+        actions = {
+            IconButton(
+                onClick = {
+                    if (!isMenuOpenValue.value) {
+                        isMenuOpenValue.value = true
+                    }
+                }
+            ) {
+                Icon(Icons.Rounded.MoreVert, "Actions")
+                Popup(
+                    Alignment.TopStart,
+                    offset = IntOffset(x = 0, y = 120),
+                    onDismissRequest = { isMenuOpenValue.value = false }
+                ) {
+                    AnimatedVisibility(
+                        isMenuOpen,
+                        enter = fadeIn() + scaleIn(),
+                        exit = fadeOut(),
+                    ) {
+                        Card(
+                            Modifier.background(Color.Transparent)
+                                .width(200.dp),
+                        ) {
+                            Column {
+                                ListItem(
+                                    modifier = Modifier
+                                        .clickable {
+
+                                        },
+                                    headlineText = {
+                                        Text("Select all")
+                                    }
+                                )
+                                ListItem(
+                                    headlineText = {
+                                        Text("Clear all")
+                                    },
+                                    modifier = Modifier
+                                        .clickable {
+
+                                        },
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    )
+}
+
 private fun LazyGridScope.section(
     component: SectionComponent,
     content: SectionState.Content,
     section: SectionModel,
-    showTitle: Boolean
+    showTitle: Boolean,
 ) {
     if (showTitle) {
         item(span = { GridItemSpan(maxCurrentLineSpan) }) {
