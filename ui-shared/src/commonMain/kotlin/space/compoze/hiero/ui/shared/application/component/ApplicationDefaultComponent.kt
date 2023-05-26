9c6ea1dfd30fca461868cf48445240efa4c3057c
Pavel <tiddiebiter@gmail.com>
2023-05-26 23:53:25 +0500

    Refactor

--- ui-shared/src/commonMain/kotlin/space/compoze/hiero/ui/shared/application/component/ApplicationDefaultComponent.kt
+++ ui-shared/src/commonMain/kotlin/space/compoze/hiero/ui/shared/application/component/ApplicationDefaultComponent.kt
@@ -2,10 +2,14 @@ package space.compoze.hiero.ui.shared.application.component
 
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.value.MutableValue
+import com.arkivanov.mvikotlin.core.binder.BinderLifecycleMode
 import com.arkivanov.mvikotlin.core.instancekeeper.getStore
 import com.arkivanov.mvikotlin.core.store.StoreFactory
+import com.arkivanov.mvikotlin.extensions.coroutines.bind
 import com.arkivanov.mvikotlin.extensions.coroutines.states
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import space.compoze.hiero.ui.shared.application.store.ApplicationStore
 import space.compoze.hiero.ui.shared.application.store.ApplicationStoreProvider
 import space.compoze.hiero.ui.shared.utils.inheritScope
 
@@ -23,9 +27,13 @@ class ApplicationDefaultComponent(
     override val state = MutableValue(store.state)
 
     init {
-        componentScope.launch {
-            store.states.collect { state.value = it }
+        bind(lifecycle, BinderLifecycleMode.CREATE_DESTROY, Dispatchers.Unconfined) {
+            store.states bindTo ::onStateChange
         }
+    }
+
+    private fun onStateChange(newState: ApplicationStore.State) {
+        state.value = newState
     }
 
 }
