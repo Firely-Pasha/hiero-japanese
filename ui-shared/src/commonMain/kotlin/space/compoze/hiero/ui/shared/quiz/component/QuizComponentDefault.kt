8faf6d45e0fe029689d45ed9ef0a477a80603459
Pavel <tiddiebiter@gmail.com>
2023-05-27 00:13:50 +0500

    Refactor

--- ui-shared/src/commonMain/kotlin/space/compoze/hiero/ui/shared/quiz/component/QuizComponentDefault.kt
+++ ui-shared/src/commonMain/kotlin/space/compoze/hiero/ui/shared/quiz/component/QuizComponentDefault.kt
@@ -2,9 +2,12 @@ package space.compoze.hiero.ui.shared.quiz.component
 
 import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.value.MutableValue
+import com.arkivanov.mvikotlin.core.binder.BinderLifecycleMode
 import com.arkivanov.mvikotlin.core.instancekeeper.getStore
 import com.arkivanov.mvikotlin.core.store.StoreFactory
+import com.arkivanov.mvikotlin.extensions.coroutines.bind
 import com.arkivanov.mvikotlin.extensions.coroutines.states
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import space.compoze.hiero.ui.shared.quiz.store.QuizStore
 import space.compoze.hiero.ui.shared.quiz.store.QuizStoreProvider
@@ -18,8 +21,6 @@ class QuizComponentDefault(
     private val items: List<Long>,
 ) : QuizComponent, ComponentContext by componentContext {
 
-    private val componentScope = inheritScope()
-
     private val store = instanceKeeper.getStore {
         QuizStoreProvider(storeFactory).create(items)
     }
@@ -27,9 +28,13 @@ class QuizComponentDefault(
     override val state = MutableValue(store.state)
 
     init {
-        componentScope.launch {
-            store.states.collect { state.value = it }
+        bind(lifecycle, BinderLifecycleMode.CREATE_DESTROY, Dispatchers.Unconfined) {
+            store.states bindTo ::onStateChange
         }
+    }
+
+    private fun onStateChange(newState: QuizStore.State) {
+        state.value = newState
     }
 
     override fun navigateBack() {
