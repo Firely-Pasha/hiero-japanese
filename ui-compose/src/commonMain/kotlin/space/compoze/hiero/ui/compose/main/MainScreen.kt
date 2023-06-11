04eee74ab5f5ee0777e7fb731886cddedaf7acab
Pavel Z <tiddiebiter@gmail.com>
2023-06-11 12:04:06 +0500

    Refactor

--- ui-compose/src/commonMain/kotlin/space/compoze/hiero/ui/compose/main/MainScreen.kt
+++ ui-compose/src/commonMain/kotlin/space/compoze/hiero/ui/compose/main/MainScreen.kt
@@ -18,41 +18,52 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.arkivanov.decompose.extensions.compose.jetbrains.stack.Children
+import com.arkivanov.decompose.router.stack.ChildStack
+import com.arkivanov.decompose.value.Value
 import space.compoze.hiero.ui.compose.stacknavigation.StackNavigator
 import space.compoze.hiero.ui.compose.utils.subscribeAsState
 import space.compoze.hiero.ui.shared.main.component.MainComponent
 
 @Composable
-fun MainScreen(component: MainComponent, modifier: Modifier = Modifier) {
+fun MainScreen(component: MainComponent) {
 
     val state by component.state.subscribeAsState()
+
+    MainScreenContent(
+        state = state,
+        childStack = component.childStack,
+        onChangeTab = component::changeTab
+    )
+}
+
+@Composable
+private fun MainScreenContent(
+    state: MainComponent.Model,
+    childStack: Value<ChildStack<*, MainComponent.Child>>,
+    onChangeTab: (tab: Int) -> Unit,
+) {
+
 
     Scaffold(
         bottomBar = {
             NavigationBar {
                 NavigationBarItem(
                     selected = state.tab == 0,
-                    onClick = {
-                        component.changeTab(0)
-                    },
+                    onClick = { onChangeTab(0) },
                     icon = {
                         Text("ひ", fontWeight = FontWeight.Bold, fontSize = 20.sp)
                     },
                 )
                 NavigationBarItem(
                     selected = state.tab == 1,
-                    onClick = {
-                        component.changeTab(1)
-                    },
+                    onClick = { onChangeTab(1) },
                     icon = {
                         Text("カ", fontWeight = FontWeight.Bold, fontSize = 20.sp)
                     },
                 )
                 NavigationBarItem(
                     selected = state.tab == 2,
-                    onClick = {
-                        component.changeTab(2)
-                    },
+                    onClick = { onChangeTab(2) },
                     icon = {
                         Icon(Icons.Outlined.Settings, "Settings")
                     },
@@ -64,8 +75,7 @@ fun MainScreen(component: MainComponent, modifier: Modifier = Modifier) {
             modifier = Modifier.padding(it)
         ) {
             Children(
-                stack = component.childStack,
-                modifier = modifier,
+                stack = childStack,
             ) {
                 when (val child = it.instance) {
                     is MainComponent.Child.Hiragana -> StackNavigator(child.component)
